20cb191bb478d608e166adf2955bddbfcbd90826
Pablo N. Diaz Acosta <pablo.diaz.acosta@gmail.com>
2016-04-13 02:58:45 -0300

    Adding flux architecture

--- source/app.jsx
+++ source/app.jsx
@@ -1,5 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Application = require('./components/Application.jsx');
+var WebAPIUtils = require('./utils/WebAPIUtils');
 
+WebAPIUtils.initializeStreamOfTweets();
 ReactDOM.render(<Application />, document.getElementById('react-application'));
